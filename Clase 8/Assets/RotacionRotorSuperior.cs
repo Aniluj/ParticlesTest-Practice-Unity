b3803b368cdbfe5a22e8486dec13ce24b38069b4
Aniluj <julianvolonterio@gmail.com>
2017-05-19 05:46:33 -0300

    Agregado de un helicoptero y rotaciones en el
    Se agregó un modelo 3d de un helicoptero y al mismo se le asignaron un
    collider y 2 scripts (uno para el rotor superior y otro para el
    fenestron).

---
+++ Clase 8/Assets/RotacionRotorSuperior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionRotorSuperior : MonoBehaviour {
+
+	public float velocidadDeRotacion;
+
+	void FixedUpdate () {
+		transform.Rotate (Vector3.forward * velocidadDeRotacion * Time.deltaTime);
+	}
+}
