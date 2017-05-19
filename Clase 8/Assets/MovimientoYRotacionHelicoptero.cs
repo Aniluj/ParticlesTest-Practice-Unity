124750628489b7ede28efd8f6d8ce4acdb721332
Aniluj <julianvolonterio@gmail.com>
2017-05-19 06:32:41 -0300

    Efecto de humo mejorado y agregado de movimientos
    Se agregó un script con todos los movimientos de traslación y rotación
    que puede realizar el helicoptero. También se mejoró el sistema de
    particulas que generan el efecto de humo, haciendo que se creen más que
    antes y se genere una sensación más realista.

---
+++ Clase 8/Assets/MovimientoYRotacionHelicoptero.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoYRotacionHelicoptero : MonoBehaviour {
+
+	public float velocidadDeMovimientoFrontalYLateral;
+	public float velocidadDeMovimientoVertical;
+	public float velocidadDeRotacion;
+
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (-Vector3.forward * velocidadDeMovimientoFrontalYLateral * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (Vector3.forward * velocidadDeMovimientoFrontalYLateral * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (-Vector3.up * velocidadDeRotacion * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (Vector3.up * velocidadDeRotacion * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (Vector3.up * velocidadDeMovimientoVertical * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (-Vector3.up * velocidadDeMovimientoVertical * Time.deltaTime);
+		}
+	}
+}
