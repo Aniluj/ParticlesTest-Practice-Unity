19a967759c92f13d3f1b12baa5071de834448f24
Aniluj <julianvolonterio@gmail.com>
2017-05-22 02:17:56 -0300

    Mecánicas de disparo agregadas y cambio en misiles
    Cambio de colores realizado en los misiles del helicoptero.
    Se agregaron 2 scripts que conforman las mecánicas de disparo y la
    destrucción de los misiles.

---
+++ Clase 8/Assets/Disparo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour {
+
+	public GameObject misil;
+	private float tiempoDeRecarga = 0f;
+	private bool primerDisparo;
+
+	void Start (){
+		primerDisparo = true;
+	}
+
+	void Update () {
+			tiempoDeRecarga += Time.deltaTime;
+		if (Input.GetKeyDown (KeyCode.Space) && primerDisparo) {
+			Instantiate (misil, transform.position, transform.rotation);
+			primerDisparo = false;
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && tiempoDeRecarga >= 1.5f && !primerDisparo) {
+			Instantiate (misil, transform.position, transform.rotation);
+			tiempoDeRecarga = 0;
+		}
+	}
+}
---
+++ Clase 8/Assets/Misil.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Misil : MonoBehaviour {
+
+	public float fuerzaDeDisparo;
+	public Transform terreno;
+
+	void Start () 
+	{
+		Rigidbody misil = GetComponent<Rigidbody> ();
+		misil.AddRelativeForce (Vector3.forward * fuerzaDeDisparo, ForceMode.Impulse);
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		Destroy (gameObject);
+	}
+}
