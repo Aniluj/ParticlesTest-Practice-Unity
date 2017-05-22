d630223ef3da17c1156d148c12d04a447f3c9116
Aniluj <julianvolonterio@gmail.com>
2017-05-22 03:13:39 -0300

    Barra de vida y condición de derrota
    Incorporación de una barra de vida hecha con 3 capsulas. También se
    agregó la condición de derrota (la nave debe perder las tres capsulas)
    que hará reiniciar el juego cuando esta ocurra.

---
+++ Clase 8/Assets/BarraDeVida.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BarraDeVida : MonoBehaviour {
+
+	public GameObject barraDeVidaParte1;
+	public GameObject barraDeVidaParte2;
+	public GameObject barraDeVidaParte3;
+	private int contadorVecesQueChoca;
+	private Scene escena;
+
+	void Start ()
+	{
+		escena = SceneManager.GetActiveScene ();
+	}
+
+	void Update () {
+		if (contadorVecesQueChoca == 1) {
+			Destroy (barraDeVidaParte1);
+		} else if (contadorVecesQueChoca == 2) {
+			Destroy (barraDeVidaParte2);
+		} else if (contadorVecesQueChoca >= 3){
+			Destroy (barraDeVidaParte3);
+			SceneManager.LoadScene (escena.name);
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Terreno") {
+			contadorVecesQueChoca++;
+		}
+	}
+}
